cbb33b2735b7f242acb4f5491acda78376177c0c
unconsolable <chenzhipeng2012@gmail.com>
2021-02-21 10:07:24 +0800

    add ecc support

--- routes/task/ecc.js
+++ routes/task/ecc.js
@@ -1,97 +1,43 @@
+// Depends on tencentcloud-sdk-nodejs version 4.0.3 or higher
+// const tencentcloud = require("tencentcloud-sdk-nodejs")
+const config = require("../../sdk/sdk.json")["tencent-ecc"]
+
 exports.route = {
   async post({title, body}) {
-    let data = {
-      title, body,
-      "Data": {
-        "Score": 72.39,
-        "ScoreCat": {
-          "Words": {
-            "Name": "词汇",
-            "Score": 76.08,
-            "Percentage": 42
-          },
-          "Sentences": {
-            "Name": "句子",
-            "Score": 61.16,
-            "Percentage": 28
-          },
-          "Structure": {
-            "Name": "篇章结构",
-            "Score": 80.37,
-            "Percentage": 23
-          },
-          "Content": {
-            "Name": "内容",
-            "Score": 69,
-            "Percentage": 7
-          },
-          "Score": 0,
-          "Percentage": 0
-        },
-        "Comment": "作者词汇基础扎实；可适当增加复合句和从句的使用；文中衔接词丰富。请多加练习，更上一层楼。",
-        "SentenceComments": [
-          {
-            "Sentence": {
-              "Sentence": "Teenagers likeating fast 1888861 food.",
-              "ParaID": 1,
-              "SentenceID": 7
-            },
-            "Suggestions": [
-              {
-                "Type": "Error",
-                "ErrorType": "拼写错误",
-                "Origin": "likeating",
-                "Replace": "elevating",
-                "Message": "likeating 可能是拼写错误，请注意拼写检查，这里应将 likeating 替换为 elevating",
-                "ErrorPosition": [
-                  8,
-                  8
-                ],
-                "ErrorCoordinate": [
-                  {
-                    "Coordinate": [
-                      424,
-                      360,
-                      459,
-                      359,
-                      459,
-                      386,
-                      424,
-                      387
-                    ]
-                  }
-                ]
-              },
-              {
-                "Type": "Error",
-                "ErrorType": "易混淆词汇建议",
-                "Origin": "1888861",
-                "Replace": "1886861",
-                "Message": "注意1888861 与 1886861 的区别，推荐将 1888861 替换为 1886861",
-                "ErrorPosition": [
-                  8,
-                  8
-                ],
-                "ErrorCoordinate": [
-                  {
-                    "Coordinate": [
-                      424,
-                      360,
-                      459,
-                      359,
-                      459,
-                      386,
-                      424,
-                      387
-                    ]
-                  }
-                ]
-              }
-            ]
-          }
-        ]
-      }
+    if (!title || !body) {
+      throw '参数不全'
     }
+    let data = config.mock
+    // const EccClient = tencentcloud.ecc.v20181213.Client
+    // const clientConfig = {
+    //   credential: {
+    //     secretId: config.secretId,
+    //     secretKey: config.secretKey
+    //   },
+    //   region: "",
+    //   profile: {
+    //     httpProfile: {
+    //       endpoint: "ecc.ap-shanghai.tencentcloudapi.com",
+    //     },
+    //   },
+    // }
+
+    // const client = new EccClient(clientConfig)
+    // const params = {
+    //   "Content": body,
+    //   "Title": title,
+    //   "Grade": "cet4"
+    // }
+    // client.ECC(params).then(
+    //   (data) => {
+    //     console.log(data)
+    //   },
+    //   (err) => {
+    //     throw err
+    //   }
+    // )
+
+
     let marks = [{Name: '总分', Score: data.Data.Score, Percentage: '100%'}]
     for (let i of Object.keys(data.Data.ScoreCat)) {
       if (data.Data.ScoreCat[i].Name && data.Data.ScoreCat[i].Score) {
